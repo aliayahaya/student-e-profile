43accd7a84af3d6e7ae15a973d94a3d8eff21529
aliayahaya <aliayahaya59@gmail.com>
2025-02-06 22:12:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
             data.forEach(profile => {
                 profileList.innerHTML += `
                     <div class="col-md-4 mb-4">
-                        <div class="card h-100 shadow-sm">
+                        <div class="card w-80 h-100 shadow-sm">
                             <img src="${profile.image}" alt="${profile.name}" class="profile-image"> <!-- Add the profile-image class here -->
                             <h3>${profile.name}</h3>
                             <div class="card-body">
